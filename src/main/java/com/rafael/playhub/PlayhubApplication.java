93a0d2ab484e8094f0017854fdd2508dbdb92313
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-02 01:11:38 +0000

    add boilerplate

---
+++ src/main/java/com/rafael/playhub/PlayhubApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.playhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/playhub/PlayhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.playhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
